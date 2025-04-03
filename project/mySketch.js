5f371e2306879f4528913681081969bc665545ed
salmasowdagarcarballo <26salma.carballo@lwhs.org>
2025-04-03 22:17:50 +0000

    flowers

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,10 +1,26 @@
 
+let data;
+let flowers = ["sunflower", "rose", "tulip"]
+let y = 50
+
+function preload() {
+  data = loadJSON("flower.json");
+}
+
 function setup() {
   createCanvas(400, 400);
   background(200);
- 
-}
-
-function draw(){
   
+  let flowers = data.flowers;
+ 
+  fill(flowers[1].r,flowers[1].g,flowers[1].b);
+  ellipse(150,150,150,150);
+  stroke(0);
+  textSize(22);
+  text(flowers[1].name, 100, 150);
+
+  for(let n = 0; n<5; n++){
+    text(flowers[f],150,y);
+    y += 50;
+  }
 }
